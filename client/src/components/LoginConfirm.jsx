14d35bda0b7bc87dd8bee7c2640debb7b7a76b92
unknown <doeh2580@gmail.com>
2024-09-19 12:24:56 +0900

    회원가입 페이지 수정, 이메일 인증 기능 미완

--- client/src/components/LoginConfirm.jsx
+++ client/src/components/LoginConfirm.jsx
@@ -1,11 +1,11 @@
-// 로그인하지 않은 사용자는 login 페이지로 리디렉션
+// 로그인하지 않은 사용자는 walletlogin 페이지로 리디렉션
 import { Navigate } from "react-router-dom";
 import { auth } from "../firebase";
 
 export default function LoginConfirm({ children }) {
   const user = auth.currentUser;
   if (user === null) {
-    return <Navigate to="/login" />;
+    return <Navigate to="/walletlogin" />;
   }
   return children;
 }
